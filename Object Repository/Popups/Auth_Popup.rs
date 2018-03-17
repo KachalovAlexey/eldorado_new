abc43e89696eb438662fc351a53bf8b21f84ca07
KachalovAlexey <kachalov123@yandex.ru>
2018-03-17 12:39:16 +0300

    1

---
+++ Object Repository/Popups/Auth_Popup.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>попап регистрации</description>
+   <name>Auth_Popup</name>
+   <tag></tag>
+   <elementGuidId>bfd4be55-518f-4805-b1f7-c19fd808543b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>#authPopupId</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Popups/Shade.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Shade</name>
+   <tag></tag>
+   <elementGuidId>d0fd06ee-a90d-4b36-9f74-12d47d52a002</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>.shadeDark</value>
+   </webElementProperties>
+</WebElementEntity>
