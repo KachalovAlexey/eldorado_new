fa35ebef29eef0fedd27166a0b2c6f1717f200c5
KachalovAlexey <kachalov123@yandex.ru>
2018-03-17 11:34:34 +0300

    тест

---
+++ Object Repository/Header/Auth_Button.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Вход</description>
+   <name>Auth_Button</name>
+   <tag></tag>
+   <elementGuidId>1b81b897-ea53-4fe7-bb48-29eb3517a27a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>.headerLoginAuth</value>
+   </webElementProperties>
+</WebElementEntity>
